17b7774610109f227ebab5e53f9d96aa5a195e62
samuel-corinthe <samuel.corinthe@hotmail.fr>
2025-10-10 09:30:40 +0200

    commit correct job-1

--- job-01/index.php
+++ job-01/index.php
@@ -1,16 +1,7 @@
 <?php
 require_once 'Product.php';
 
-$product = new Product(
-    1,
-    'T-shirt',
-    ['https://picsum.photos/200/300'],
-    1000,
-    'A beautiful T-shirt',
-    10,
-    new DateTime(),
-    new DateTime()
-);
+$product = new Product( 1,'T-shirt',['https://picsum.photos/200/300'],1000,'A beautiful T-shirt',10,new DateTime(),new DateTime());
 
 var_dump($product->getName());
 $product->setName('Sweat-shirt');
